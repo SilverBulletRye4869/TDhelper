16e817c4ff7a9d0476b7355eb351ee1b51e6b757
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-04-11 19:25:39 +0900

    delete関数の追加、spawn時yawが反映されるように変更

--- src/main/java/net/serveron/hane/tdhelper/system/MainSystem.java
+++ src/main/java/net/serveron/hane/tdhelper/system/MainSystem.java
@@ -12,13 +12,13 @@ import java.util.List;
 import java.util.Map;
 
 public class MainSystem {
-    private static final YamlConfiguration YML = CustomConfig.getYmlByID("data");
-
     private final JavaPlugin plugin;
     private final Map<String, TDgroup> TDgroupMap = new HashMap<>();
+    private final YamlConfiguration YML;
 
     public MainSystem(JavaPlugin plugin){
         this.plugin = plugin;
+        YML = CustomConfig.getYmlByID("data");
     }
 
     public TDgroup getTDunitByID(String id){
@@ -29,16 +29,26 @@ public class MainSystem {
         return TDgroupMap.get(id);
     }
 
-    public TDgroup createIfNotExists(Location loc, String id){
+    public TDgroup createIfNotExists(Location loc, String id){return createIfNotExists(loc,id,null);}
+    public TDgroup createIfNotExists(Location loc, String id, String init){
         if(TDgroupMap.containsKey(id))return TDgroupMap.get(id);
-        String uuidStr = this.spawnNew(loc).getUniqueId().toString();
+        TextDisplay td = this.spawnNew(loc);
+        if(init!=null)td.setText(init);
+        String uuidStr = td.getUniqueId().toString();
         YML.set(id+".uuids", List.of(uuidStr));
         CustomConfig.saveYmlByID("data");
         return getTDunitByID(id);
     }
 
+    public void delete(String id){
+        TDgroupMap.get(id).delete();
+        TDgroupMap.remove(id);
+    }
+
     public TextDisplay spawnNew(Location loc){
-        return (TextDisplay) loc.getWorld().spawnEntity(loc, EntityType.TEXT_DISPLAY);
+        TextDisplay td  = (TextDisplay) loc.getWorld().spawnEntity(loc, EntityType.TEXT_DISPLAY);
+        td.setRotation(loc.getYaw(),0);
+        return td;
     }
 }
 
