6509f9a8df91c155695160de62823ff75811163d
SilverBullet_Rye <silverbulletrye4869@gmail.com>
2023-04-12 18:37:43 +0900

    ymlの名前を変数管理

--- src/main/java/net/serveron/hane/tdhelper/TDhelper.java
+++ src/main/java/net/serveron/hane/tdhelper/TDhelper.java
@@ -7,6 +7,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.util.logging.Logger;
 
 public final class TDhelper extends JavaPlugin {
+    public static final String NORMAL_DATA = "data";
+    public static final String SPECIAL_DATA = "sp_data";
+
     private static JavaPlugin plugin = null;
     private static Logger log = null;
     private static MainSystem mainSystem = null;
@@ -17,7 +20,8 @@ public final class TDhelper extends JavaPlugin {
         log = getLogger();
 
         this.saveDefaultConfig();
-        CustomConfig.createYmlByID("data");
+        CustomConfig.createYmlByID(NORMAL_DATA);
+        CustomConfig.createYmlByID(SPECIAL_DATA);
 
         mainSystem = new MainSystem(this);
         new Command(this,mainSystem);
